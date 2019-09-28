06a661a58cc017c4cf90672a13a413103e3b32ac
qw-Zhang <411026539@qq.com>
2019-09-28 09:58:14 +0800

    deleted_some_file

--- ch7/ex_7_6.c
+++ ch7/ex_7_6.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-
+#include<string.h>
 void written_amount(unsigned int amount, char *buffer);
 
 int main(void)
@@ -22,7 +22,19 @@ void written_amount(unsigned int amount, char *buffer)
         temp/=10; 
     }
     int dot_num = bit_num / 3;
-
+    int r_num = bit_num % 3;
+    char str_dot[3][100] = {{"HUNDRED"},{"THOUSAND"},{"MILLION"}};
+    char str_sec_num[9][100] = {{"TENTY"},{"TWENTY"},{"THIRTY"},{"FOURTY"},{"FIVTY"},{"SIXTY"},{"SEVENTY"},
+                                {"EIGHTY"},{"NINTY"}};
+    char str_num[9][100] = {{"ONE"},{"TWO"},{"THREE"},{"FOUR"},{"FIVE"},{"SIX"},{"SEVEN"},{"EIGHT"},{"NINE"}};
+    temp = amount;
+    int ttt = 0;
+    for(int i = 0;i < dot_num;i++){
+        ttt = temp%10;
+        if(ttt>0)
+            str_temp = str_num[ttt];
+        
+    } 
 }
 
 
