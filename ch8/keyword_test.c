273196604ac890910e9ba943a4def1c71d732fcb
qw-Zhang <411026539@qq.com>
2019-09-29 16:04:48 +0800

    update_vim_configuration

--- ch8/keyword_test.c
+++ ch8/keyword_test.c
@@ -1,6 +1,6 @@
 #include"keyword.h"
 #include<stdio.h>
-
+#include<math.h>
 char const *keyword[] = {
     "do",
     "for",
@@ -18,5 +18,7 @@ int main(void)
     char *desire ="which";
     int p = lookup_keyword(desire,keyword,N_KEYWORD);
     printf("%d\n",p);
+    double rr = sqrt(2);
+    printf("%f\n",rr);
     return 0;
 }
